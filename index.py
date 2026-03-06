1c997bdca34b35b80e79f4274e0b72e939d96e91
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-06 22:52:35 +0530

    new learnings

---
+++ index.py
@@ -0,0 +1,31 @@
+print("Hello Jacksy, welcome to AI development!")
+name = "Jacksy"
+age = 30
+is_developer = True
+height = 5.9
+
+print(name)
+print(age)
+print(is_developer)
+print(type(height))
+print(type(name))
+print(type(age))
+print(type(is_developer))
+
+newname= input("please enter your name")
+print("hello " + newname)
+students=["jacksy","kanha","jps","prakash"]
+print(students)
+print(students[3])
+students.append("jyoti")
+print(students)
+person = {
+    "name": "Jacksy",
+    "age": 30,
+    "role": "developer"
+}
+
+print(person["name"])
+print(person["role"])
+person["country"]="india"
+print(person)
