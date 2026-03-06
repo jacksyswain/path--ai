451739a2793a55622f4f0b779ad4c57fbcdbd493
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-06 23:29:17 +0530

    new learnings

---
+++ day2.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    print(i)
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+
+gosti={
+    "kanha":31,
+    "biki":29,
+    "bapu":38,
+    "jagu":39
+}
+
+for key in gosti :
+    if key=="kanha":
+        print(gosti[key])
