0ec47ac1ad7386d2c3a9b0b7a547226fefc1dcf4
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-07 23:35:03 +0530

    new learnings

--- day3.py
+++ day3.py
@@ -1,8 +1,28 @@
-name = input("hey can yoy share your name ")
-age = int(input("hey would you like to share your age "))
-corse= input("hey can you share the corse you are intrest in ")
+import random
+import math
+
+print(math.sqrt(16))
+
+print(random.randint(1,10))
+
+name = input("hey can yoy share your name: ")
+age = int(input("hey would you like to share your age: "))
+corse= input("hey can you share the corse you are intrest in: ")
 
 def greet(name,age,corse ):
     print("hello ", name , " you are ", age, "years old and as we know you are intrested on ", corse )
 
-greet(name,age,corse)
+greet(name,age,corse) 
+
+def square(num):
+    return num*num
+
+print(square(9))
+
+def check_even(age):
+    if age%2==0:
+        print("you are at even age")
+    else:
+        print("you are at odd age ")
+
+check_even(age)
