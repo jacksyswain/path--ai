e42ee68fbf4ba049a0ed2b3b18f3a707784dae3c
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-08 20:23:50 +0530

    new learnings

---
+++ day4.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
+
+data = response.json()
+
+user = data["results"][0]
+
+print("Name:", user["name"]["first"])
+print("Email:", user["email"])
+print("Country:", user["location"]["country"])
