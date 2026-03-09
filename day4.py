a19e9f8c8ec137c24980c27576e247b274c5a8b1
jyotiprakash swain <jp.swainabc@gmail.com>
2026-03-09 23:19:16 +0530

    new learnings

--- day4.py
+++ day4.py
@@ -9,3 +9,6 @@ user = data["results"][0]
 print("Name:", user["name"]["first"])
 print("Email:", user["email"])
 print("Country:", user["location"]["country"])
+print("Name:", user["name"]["first"])
+print("Email:", user["email"])
+print("Country:", user["location"]["country"])
